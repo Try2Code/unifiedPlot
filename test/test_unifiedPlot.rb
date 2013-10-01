2c9fe0dc2661e946146b474549bdb151fddb691b
Ralf Mueller <ralf.mueller@zmaw.de>
2013-10-01 17:58:01 +0200

    Check Array,NArray and GSL::Vector input

--- test/test_unifiedPlot.rb
+++ test/test_unifiedPlot.rb
@@ -3,51 +3,78 @@ require 'minitest/autorun'
 require "unifiedPlot"
 require 'narray'
 require 'gsl'
-require "pp"
+require 'pp'
 
 class TestPlotter < Minitest::Test
-  def test_single_plot
-    x = (0...10).to_a.map(&:to_f)
+  def setup
+    size = ENV.has_key?('SIZE') ? ENV['SIZE'] : 10
+    x = (0...size).to_a.map(&:to_f)
     y = [1,2,3,4,5,4,3,2,4,0].map(&:to_f)
-    data = {
-      :x => NArray[*x],
-      :y => NArray[*y],
-    }
-    UnifiedPlot.linePlot([data])
+
+    allowedTypes = ["narray", "gsl", "ruby"]
+    @inputType = ENV.has_key?('itype') ? ENV['itype'].downcase : 'narray'
+    unless allowedTypes.include?(@inputType) then
+      warn "Wrong inputtype #{@inputType}!"
+      warn "Use one of #{allowedTypes.join(',')}."
+      exit(1)
+    end
+    @data = case @inputType
+    when 'narray' then
+      {
+        :x => NArray[*x],
+        :y => NArray[*y],
+      }
+    when 'ruby' then
+      {
+        :x => x,
+        :y => y,
+      }
+    when 'gsl' then
+      {
+        :x => GSL::Vector[*x],
+        :y => GSL::Vector[*y],
+      }
+    end
+    pp @data
+  end
+  def mathOp(op,input)
+    case @inputType
+    when 'gsl' then
+      input.send(op)
+    when 'narray' then
+      NMath.send(op,input)
+    when 'ruby' then
+      input.map {|v| Math.send(op,v)}
+    end
+  end
+  def test_single_plot
+    UnifiedPlot.linePlot([@data])
   end
   def test_single_plot_noX
-    y = [1,2,3,4,5,4,3,2,4,0].map(&:to_f)
     data = {
-      :y => NArray[*y],
+      :y => @data[:y],
       :title => 'noX',
     }
     UnifiedPlot.linePlot([data],title: 'noX')
   end
   def test_multi_plot_with_title
-    x = (0...10).to_a.map(&:to_f)
-    y = [1,2,3,4,5,4,3,2,4,0].map(&:to_f)
-    data = [{
-      :x => NArray[*x],
-      :y => NArray[*y],
-      :title => 'a',
-    },{
-      :x => NArray[*x],
-      :y => NMath.sin(NArray[*y]),
-      :title => 'sin(a)',
-    }]
+    data = [@data.merge({ :title => 'a' }),
+            {
+              :x => @data[:x],
+              :y => mathOp(:sin,@data[:y]),
+              :title => 'sin(a)',
+            }]
     UnifiedPlot.linePlot(data)
   end
   def test_multi_plot_with_axes
-    x = (0...10).to_a.map(&:to_f)
-    y = [1,2,3,4,5,4,3,2,4,0].map(&:to_f)
     data = [{
-      :x => NArray[*x],
-      :y => NArray[*y],
-      :axes => 'x1y2',
-      :title => 'x1y2',
+      :x => @data[:x],
+      :y => @data[:y],
+      :axes => 'x2y1',
+      :title => 'x2y1',
     },{
-      :x => NArray[*x],
-      :y => NMath.sin(NArray[*y]),
+      :x => @data[:x],
+      :y => mathOp(:sin,@data[:y]),
       :axes => 'x2y2',
       :title => 'x2y2',
 
@@ -55,33 +82,31 @@ class TestPlotter < Minitest::Test
     UnifiedPlot.linePlot(data)
   end
   def test_multi_plot_with_labels
-    x = (0...10).to_a.map(&:to_f)
-    y = [1,2,3,4,5,4,3,2,4,0].map(&:to_f)
     data = [{
-      :x => NArray[*x],
-      :y => NArray[*y],
+      :x => @data[:x],
+      :y => @data[:y],
       :axes => 'x1y2',
       :title => 'x1y2',
     },{
-      :x => NMath.cos(NArray[*x]),
-      :y => NMath.sin(NArray[*y]),
+      :x => mathOp(:cos,@data[:x]),
+      :y => mathOp(:sin,@data[:y]),
       :axes => 'x2y2',
       :title => 'x2y2',
 
     }]
     plotConf = {:y2label => 'y2label',:x2label => 'x2label'}
-    UnifiedPlot.linePlot(data,plotConf)
+    UnifiedPlot.linePlot(data,plotConf: plotConf)
   end
   def test_multi_plot_with_label_pos
     x = (0...10).to_a.map(&:to_f)
     y = [1,2,3,4,5,4,3,2,4,0].map(&:to_f)
     data = [{
-      :x => NArray[*x],
-      :y => NArray[*y],
+      :x => @data[:x],
+      :y => @data[:y],
       :axes => 'x1y2',
       :title => 'x1y2',
     },{
-      :y => NMath.sin(NArray[*y]),
+      :y => mathOp(:sin,@data[:y]),
       :title => 'sin',
 
     }]
